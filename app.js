7e1e286393dc6784fca224173b7b3521e12f7581
shaheer-67 <shaheerali2076@gmail.com>
2025-03-17 09:46:53 +0500

    Initial commit

--- app.js
+++ app.js
@@ -1,40 +1,4 @@
- 
-// //  arrow function 
-// const arrows = document.querySelectorAll(".arrow");
-// const movieLists = document.querySelectorAll(".movie-list");
 
-// arrows.forEach((arrow, i) => {
-//   const itemNumber = movieLists[i].querySelectorAll("img").length;
-//   let clickCounter = 0;
-//   arrow.addEventListener("click", () => {
-//     clickCounter++;
-//     if (itemNumber - (4 + clickCounter) >= 0) {
-//       movieLists[i].style.transform = `translateX(${
-//         movieLists[i].computedStyleMap().get("transform")[0].x.value - 300
-//       }px)`;
-//     } else {
-//       movieLists[i].style.transform = "translateX(0)";
-//       clickCounter = 0;
-//     }
-//   });
-//   console.log(movieLists[i].querySelectorAll("img").length);
-// });
-
-
-
-// // toggle button
-// const ball = document.querySelector(".toggle-ball");
-// const items = document.querySelectorAll(
-//   ".container ,.sidebar ,.left-menu-icons ,.navbar-container ,.movie-list-title ,.toggle"
-// );
-
-// ball.addEventListener("click", () => {
-//   items.forEach((item) => {
-//     item.classList.toggle("active");
-//   });
-
-//   ball.classList.toggle("active");
-// });
 
 
 
